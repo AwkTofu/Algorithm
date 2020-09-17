bdbf273a1e29531cffd549b886af6241e386bda7
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-09-17 15:54:29 -0400

    Starting Terminal Search

--- test.js
+++ test.js
@@ -1,37 +1,6 @@
-//let testArr = [1, 3, 5, 7, 8, 9, 20, 42, 64]
-let testMoney = 6.27;
+let testArr = [1, 3, 5, 7, 8, 9, 20, 42, 64]
 
-let bills = {
-	hundredDollar: 100.0,
-	tenDollar: 10.0,
-	fiveDollar: 5.0,
-	oneDollar: 1.0,
-	quarter: 0.25,
-	dime: 0.1,
-	nickel: 0.05,
-	penny: 0.01
+
+let ternarySearch = (let, right, target, arr) => {
+	
 }
-
-let FindingChange = (currency, amount) => {
-	let resultBills = {};
-	let cashLeftover = amount;
-
-	for (let key in currency)
-	{
-		while (cashLeftover >= currency[key])
-		{
-			if (resultBills[key])
-			{
-				resultBills[key] += 1;
-			}
-			else
-			{
-				resultBills[key] = 1;
-			}
-			cashLeftover = (cashLeftover - currency[key]).toFixed(2)
-		}
-	}
-	return resultBills;
-}
-
-FindingChange(bills, testMoney);
